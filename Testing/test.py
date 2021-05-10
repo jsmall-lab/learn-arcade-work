36f99244d18408dba3e421297dd4927ae86a5f93
jsmall-lab <82071548+jsmall-lab@users.noreply.github.com>
2021-05-10 16:57:13 +1200

    programs

--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,9 @@
 for row in range(10):
     for i in range(10 - row):
         print("", end=' ')
-    for i in range(1, row ):
-        print(i, end=" ")
+    for i in range(1, row  ):
+        print(i, end="")
+   
        
         
       
