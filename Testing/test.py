25ce53b2f4b63baffa39ea15c468833b72f62b2f
jsmall <jsmall@stu.kerikerihigh.ac.nz>
2021-05-11 12:14:29 +1200

    programs

--- Testing/test.py
+++ Testing/test.py
@@ -1,15 +1,11 @@
-for row in range(10):
+for row in range(11):
     for i in range(10 - row):
-        print("", end=' ')
-    for i in range(1, row  ):
-        print(i, end="")
+        print(" ", end=' ')
+    for i in range(1, row ):
+        print(i, end= " ")
+    for i in range(i - 1, 0, -1):
+        print(i, end=" ")
    
-       
-        
-      
-    
-    
-    
     print()
     
     
