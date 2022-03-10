0620830c4bdb6e3718efc6b58af38daa31cb44b8
jsmall <jsmall@stu.kerikerihigh.ac.nz>
2022-03-10 13:18:22 +1300

    added bullet sprite
    added rotational deceleration

--- practice_game_year_13/Game_test.py
+++ practice_game_year_13/Game_test.py
@@ -5,10 +5,12 @@ HEIGHT = 800
 TITLE = "test"
 
 CHARACTER_SCAILING = 2
+BULLET_SCAILING = 1
 
 PLAYER_ACCELERATION = 0.05
 PLAYER_DEACCELERATION = 0.02
 PLAYER_CHANGE_ANGLE_SPEED = 3
+PLAYER_ANGLE_DECCELERATION = 0.03
 
 class TestGame(arcade.Window):
     def __init__(self):
@@ -28,6 +30,10 @@ class TestGame(arcade.Window):
         self.accelerating_left = None
         self.accelerating_right = None
         self.moving = None
+        self.moving_angle = None
+
+        self.player_bullet = None
+        self.player_bullet_list = None
 
         arcade.set_background_color(arcade.color.BLACK)
 
@@ -38,18 +44,22 @@ class TestGame(arcade.Window):
         self.scene = arcade.Scene()
         self.scene.add_sprite_list("player")
 
+        self.player_bullet_list = arcade.SpriteList()
+
         image_source = "practice_game_year_13/stuff/main_character.png.png" 
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCAILING)
         self.player_sprite.center_x = 500
         self.player_sprite.center_y = 400
         self.scene.add_sprite("player", self.player_sprite)
 
+        
         self.player_speed = 0
         self.accelerating_up = False
         self.accelerating_down = False
         self.accelerating_left = False
         self.accelerating_right = False
         self.moving = False
+        self.moving_angle = False
 
     def on_draw(self):
         self.clear()
@@ -67,7 +77,7 @@ class TestGame(arcade.Window):
             self.player_sprite.change_y += PLAYER_ACCELERATION
         if self.accelerating_down == True:
             self.player_sprite.change_y -= PLAYER_ACCELERATION
-        if self.moving == False:
+        if not self.moving:
             if self.player_sprite.change_x > 0:
                 self.player_sprite.change_x -= PLAYER_DEACCELERATION
             elif self.player_sprite.change_x < 0:
@@ -76,7 +86,12 @@ class TestGame(arcade.Window):
                 self.player_sprite.change_y -= PLAYER_DEACCELERATION
             elif self.player_sprite.change_y < 0:
                 self.player_sprite.change_y += PLAYER_DEACCELERATION
-        
+        if not self.moving_angle:
+            if self.player_sprite.change_angle < 0:
+                self.player_sprite.change_angle += PLAYER_ANGLE_DECCELERATION
+            if self.player_sprite.change_angle > 0:
+                self.player_sprite.change_angle -= PLAYER_ANGLE_DECCELERATION
+                
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.W:
@@ -93,8 +108,18 @@ class TestGame(arcade.Window):
             self.moving = True
         if key == arcade.key.LEFT:
             self.player_sprite.change_angle = PLAYER_CHANGE_ANGLE_SPEED
+            self.moving_angle = True
         if key == arcade.key.RIGHT:
             self.player_sprite.change_angle = -PLAYER_CHANGE_ANGLE_SPEED
+            self.moving_angle = True
+        if key == arcade.key.SPACE:
+            image_source = "practice_game_year_13/stuff/laser.png"
+            self.bullet = arcade.Sprite(image_source, BULLET_SCAILING)
+            self.bullet.center_x = (self.player_sprite.center_x + 10)
+            self.bullet.center_y = self.player_sprite.center_y
+            self.bullet.change_x = 3
+            self.player_bullet_list.append(self.bullet)
+            self.scene.add_sprite_list("bullets", self.player_bullet_list)
 
     
     def on_key_release(self, key, modifiers):
@@ -111,9 +136,9 @@ class TestGame(arcade.Window):
             self.accelerating_left = False
             self.moving = False
         if key == arcade.key.LEFT:
-            self.player_sprite.change_angle -= PLAYER_CHANGE_ANGLE_SPEED
+            self.moving_angle = False
         if key == arcade.key.RIGHT:
-            self.player_sprite.change_angle -= -PLAYER_CHANGE_ANGLE_SPEED
+            self.moving_angle = False
 def main():
     window = TestGame()
     window.setup()
